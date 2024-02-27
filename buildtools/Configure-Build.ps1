f3f608d66f43af5ebb7bb7e24d67dc05323764bf
Doug Slater <dousla@raysearchlabs.com>
2024-02-27 17:08:53 -0500

    Make builds reproducible

--- buildtools/Configure-Build.ps1
+++ buildtools/Configure-Build.ps1
@@ -3,8 +3,8 @@ param (
 )
 
 # Update these lines for other versions of Visual Studio
-#$vc = "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build"
-$vc = "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build"
+$vc = "C:/Program Files/Microsoft Visual Studio/2022LTSC176/Enterprise/VC/Auxiliary/Build"
+#$vc = "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build"
 $cmakeGenerator = "Visual Studio 17 2022"
 
 # Optionally replace these with absolute paths
--- buildtools/Start-MSBuild.ps1
+++ buildtools/Start-MSBuild.ps1
@@ -2,12 +2,13 @@
 
 # Invoke MSBuild on the given solution with the given configuration
 # Assumes MSBuild is in the path.
-function Invoke-Msbuild {
+function Start-MSBuild{
   param(
     [String] $configuration,
     [String] $solution,
-    [bool]$parallelTargets = $True
+    [bool] $parallelTargets = $True
   )
+  echo "booger"
 
   # /m flag means build targets with all processors. 
   # Note that this only provides target level parallelism. 
